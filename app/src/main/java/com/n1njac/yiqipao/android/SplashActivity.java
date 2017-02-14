1f8e58ed7cfadb2c6a4a8e4564708c7b73ba0d93
N1njaC <aiai173cc@gmail.com>
2017-02-14 22:36:05 +0800

    update

---
+++ app/src/main/java/com/n1njac/yiqipao/android/SplashActivity.java
@@ -0,0 +1,16 @@
+package com.n1njac.yiqipao.android;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by huanglei on 2017/2/14.
+ */
+
+public class SplashActivity extends AppCompatActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
---
+++ app/src/main/java/run/TransitionActivity.java
@@ -0,0 +1,16 @@
+package run;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by huanglei on 2017/2/14.
+ */
+
+public class TransitionActivity extends AppCompatActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
