a638e7b7178ff915ef36083f7a4df548f7d12389
N1njaC <aiai173cc@gmail.com>
2017-03-13 11:15:45 +0800

    z

---
+++ app/src/main/java/com/n1njac/yiqipao/android/login/BaseActivty.java
@@ -0,0 +1,15 @@
+package com.n1njac.yiqipao.android.login;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by huanglei on 2017/3/13.
+ */
+
+public class BaseActivty extends AppCompatActivity {
+    @Override
+    protected void onCreate( Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
---
+++ app/src/main/java/com/n1njac/yiqipao/android/login/LoginActivity.java
@@ -0,0 +1,8 @@
+package com.n1njac.yiqipao.android.login;
+
+/**
+ * Created by huanglei on 2017/3/13.
+ */
+
+public class LoginActivity {
+}
