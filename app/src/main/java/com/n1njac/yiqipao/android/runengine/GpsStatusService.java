0c315549f7898b81e6237321512df5a4f2cceb7e
CCCCoder <15505514373@163.com>
2017-09-17 22:34:07 +0800

    添加gps服务

--- app/src/main/java/com/n1njac/yiqipao/android/runengine/GpsStatusService.java
+++ app/src/main/java/com/n1njac/yiqipao/android/runengine/GpsStatusService.java
@@ -2,19 +2,31 @@ package com.n1njac.yiqipao.android.runengine;
 
 import android.app.Service;
 import android.content.Intent;
+import android.os.Binder;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
 
-/**
- * Created by N1njaC on 2017/9/16.
+/*
+ *    Created by N1njaC on 2017/9/16.
+ *    email:aiai173cc@gmail.com
  */
 
 public class GpsStatusService extends Service {
 
 
+    private IGPSStatusCallback mCallback;
+
+
     @Nullable
     @Override
     public IBinder onBind(Intent intent) {
-        return null;
+        return new GpsBinder();
+    }
+
+    private class GpsBinder extends Binder {
+
+        public GpsStatusService getGpsStatusService() {
+            return GpsStatusService.this;
+        }
     }
 }
